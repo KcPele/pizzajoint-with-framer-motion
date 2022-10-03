865c2324d42df1ead3c67e1c413a81dc66999f7b
KcPele <fidekg123@gmail.com>
2022-10-03 22:53:48 +0100

    done

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -6,7 +6,19 @@ const bacdropVarients = {
   visible: { opacity: 1 },
   hidden: { opacity: 0 },
 };
-const Modal = ({ showModal, setShowModal }) => {
+
+const modal = {
+  hidden: {
+    y: "-100vh",
+    opacity: 0,
+  },
+  visible: {
+    y: "200px",
+    opacity: 1,
+    transition: { delay: 0.5 },
+  },
+};
+const Modal = ({ showModal }) => {
   return (
     <AnimatePresence exitBeforeEnter>
       {showModal && (
@@ -15,9 +27,14 @@ const Modal = ({ showModal, setShowModal }) => {
           variants={bacdropVarients}
           animate="visible"
           initial="hidden"
+          exit="hidden"
         >
-
-          
+          <motion.div className="modal" variants={modal}>
+            <p>Wants to make another Pizza</p>
+            <Link to="/">
+              <button>Start again</button>
+            </Link>
+          </motion.div>
         </motion.div>
       )}
     </AnimatePresence>
